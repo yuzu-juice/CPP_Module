54b9d91e96faf33329758682e3e9ec9630781077
yuzu-juice <yuzujuicesec@gmail.com>
2025-08-27 11:23:16 +0900

    wip

--- 00/ex00/main.cpp
+++ 00/ex00/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 	if (argc == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return 0;
+		return (0);
 	}
 	for (int i = 1; i < argc; i++)
 	{
