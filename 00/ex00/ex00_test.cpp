c09a5b899d20c602279a355c79ffcac33b81db93
yuzu-juice <yuzujuicesec@gmail.com>
2025-08-30 15:06:44 +0900

    add: some settings

--- 00/ex00/ex00_test.cpp
+++ 00/ex00/ex00_test.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+
 #include "main.h"
 
 TEST(MegaphoneTest, BasicAssertions) {
@@ -7,12 +8,9 @@ TEST(MegaphoneTest, BasicAssertions) {
   EXPECT_EQ("", megaphone(""));
   EXPECT_EQ("___", megaphone("___"));
   EXPECT_EQ("_A!_'_A", megaphone("_A!_'_a"));
+  EXPECT_EQ("SHHHHH... I THINK THE STUDENTS ARE ASLEEP...",
+            megaphone("shhhhh... I think the students are asleep..."));
   EXPECT_EQ(
-    "SHHHHH... I THINK THE STUDENTS ARE ASLEEP...",
-    megaphone("shhhhh... I think the students are asleep...")
-  );
-  EXPECT_EQ(
-    "DAMNIT ! SORRY STUDENTS, I THOUGHT THIS THING WAS OFF.",
-    megaphone("Damnit ! Sorry students, I thought this thing was off.")
-  );
+      "DAMNIT ! SORRY STUDENTS, I THOUGHT THIS THING WAS OFF.",
+      megaphone("Damnit ! Sorry students, I thought this thing was off."));
 }
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # CPP_Module
 
-[![cpp-linter](https://github.com/cpp-linter/cpp-linter-action/actions/workflows/cpp-linter.yml/badge.svg)](https://github.com/cpp-linter/cpp-linter-action/actions/workflows/cpp-linter.yml)
+[![cpp-linter](https://github.com/yuzu-juice/CPP_Module/actions/workflows/cpp-linter.yml/badge.svg)](https://github.com/yuzu-juice/CPP_Module/actions/workflows/cpp-linter.yml)
