efe04c40d9c953103e38ef5d036a8cf831f170ee
yuzu-juice <yuzujuicesec@gmail.com>
2025-10-09 22:07:25 +0900

    update: constructor:

--- 01/ex00/Zombie.cpp
+++ 01/ex00/Zombie.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 
-Zombie::Zombie(std::string name) : _name(name) {}
+Zombie::Zombie(std::string name) { this->_name = name; }
 
 Zombie::~Zombie() { std::cout << this->_name << " is dead." << std::endl; }
 
