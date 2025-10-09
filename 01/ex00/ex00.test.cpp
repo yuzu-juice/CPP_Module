3881f5b930f0e88a3674db4df864591b0e565b35
yuzu-juice <yuzujuicesec@gmail.com>
2025-10-09 22:06:06 +0900

    del: test file

--- 01/ex00/ex00.test.cpp
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-
-#include "Zombie.hpp"
-
-TEST(ZombieTest, BasicAssertions) {
-  Zombie test_zom("Foo");
-  EXPECT_EQ("Foo:  BraiiiiiiinnnzzzZ...", test_zom.annouce());
-}
