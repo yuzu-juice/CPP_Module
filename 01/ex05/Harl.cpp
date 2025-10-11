211fe0d4c25dda4beaf5ecf350e6467abb4bbf06
yuzu-juice <yuzujuicesec@gmail.com>
2025-10-11 14:51:03 +0900

    a

--- 01/ex05/Harl.cpp
+++ 01/ex05/Harl.cpp
@@ -7,7 +7,7 @@ Harl::Harl() {}
 Harl::~Harl() {}
 
 void Harl::complain(std::string level) {
-  void (Harl::* fptrs[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning,
+  void (Harl::*fptrs[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning,
                                   &Harl::error};
   std::string complains[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
