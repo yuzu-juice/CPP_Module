8997b06b9c5211261e40aac6389ceaf029d8ea02
yuzu-juice <yuzujuicesec@gmail.com>
2025-10-12 20:35:21 +0900

    finsh

--- 02/ex01/Fixed.hpp
+++ 02/ex01/Fixed.hpp
@@ -1,18 +1,28 @@
 #ifndef FIXED_HPP_
 #define FIXED_HPP_
 
+#include <ostream>
+
 class Fixed {
  public:
   Fixed();
   Fixed(const Fixed &);
-  Fixed &operator=(const Fixed &);
+  Fixed(const int);
+  Fixed(const float);
   ~Fixed();
+
+  Fixed &operator=(const Fixed &);
+
   int getRawBits() const;
   void setRawBits(int const);
+  float toFloat() const;
+  int toInt() const;
 
  private:
   int value;
   static const int fraction_bits = 8;
 };
+
+std::ostream &operator<<(std::ostream &, const Fixed &);
 
 #endif
