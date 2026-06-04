5c9235460923739926fd4ac39cbcbe37d16e9b04
yuzu-juice <yuzujuicesec@gmail.com>
2026-06-04 11:21:31 +0900

    update: print form detail

--- 05/ex03/main.cpp
+++ 05/ex03/main.cpp
@@ -9,13 +9,17 @@
 
 int main() {
   Intern someRandomIntern;
-  AForm *shruberry, *robotomy, *presidential;
+  AForm *shruberry, *robotomy, *presidential, *other;
 
   shruberry = someRandomIntern.makeForm("shruberry creation", "target");
-  robotomy = someRandomIntern.makeForm("rotobomy creation", "target");
+  robotomy = someRandomIntern.makeForm("robotomy request", "target");
   presidential = someRandomIntern.makeForm("presidential pardon", "target");
+  other = someRandomIntern.makeForm("other form", "target");
 
-  // TODO: Print form detail
+  std::cout << *shruberry << std::endl;
+  std::cout << *robotomy << std::endl;
+  std::cout << *presidential << std::endl;
+  std::cout << other << std::endl;
 
   delete shruberry;
   delete robotomy;
