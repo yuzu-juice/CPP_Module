126cc28edd35bca35741af1a088d1ebdab76faab
yuzu-juice <yuzujuicesec@gmail.com>
2026-07-27 01:04:03 +0900

    impl

--- 07/ex00/whatever.hpp
+++ 07/ex00/whatever.hpp
@@ -1,4 +1,21 @@
 #ifndef WHATEVER_HPP_
 #define WHATEVER_HPP_
 
+template <typename T>
+void swap(T& first, T& second) {
+  T temporary = first;
+  first = second;
+  second = temporary;
+}
+
+template <typename T>
+const T& min(const T& first, const T& second) {
+  return first < second ? first : second;
+}
+
+template <typename T>
+const T& max(const T& first, const T& second) {
+  return first > second ? first : second;
+}
+
 #endif
--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -1,3 +1,31 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+
 #include "iter.hpp"
 
-int main() { return 0; }
+template <typename T>
+void increment(T& value) {
+  ++value;
+}
+
+template <typename T>
+void print(const T& value) {
+  std::cout << value << std::endl;
+}
+
+int main() {
+  int numbers[] = {1, 2, 3};
+
+  ::iter(numbers, 3, increment<int>);
+  assert(numbers[0] == 2);
+  assert(numbers[1] == 3);
+  assert(numbers[2] == 4);
+  ::iter(numbers, 3, print<int>);
+
+  const std::string words[] = {"one", "two"};
+
+  ::iter(words, 2, print<std::string>);
+
+  return 0;
+}
