146d5aa22dd967f874f1fb13ef4a4b9eef32f2a9
yuzu-juice <yuzujuicesec@gmail.com>
2026-08-31 03:12:36 +0000

    Keep ex02 tests while aligning names

--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -11,19 +11,26 @@ int main() {
   mstack.pop();
   std::cout << mstack.size() << std::endl;
   mstack.push(3);
-  mstack.push(5);
-  mstack.push(737);
+  mstack.push(7);
   mstack.push(0);
 
   MutantStack<int>::iterator it = mstack.begin();
-  MutantStack<int>::iterator ite = mstack.end();
-  ++it;
-  --it;
-  while (it != ite) {
-    std::cout << *it << std::endl;
-    ++it;
+  std::cout << *it << std::endl;
+  *it = 6;
+  std::cout << *mstack.begin() << std::endl;
+
+  const MutantStack<int> copy(mstack);
+  int index = 0;
+  for (MutantStack<int>::const_iterator current = copy.begin();
+       current != copy.end(); ++current) {
+    std::cout << *current << std::endl;
+    ++index;
   }
+  std::cout << index << std::endl;
 
   std::stack<int> s(mstack);
+  std::cout << s.top() << std::endl;
+  std::cout << s.size() << std::endl;
+
   return 0;
 }
